687a864e86121a18f987d27ddec54998e99ce58d
Naresh0348 <nareshbaskar11@gmail.com>
2025-06-11 19:17:59 +0530

    Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Home from './Page/Home';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Expform from './Components/expform';
 
 function App() {
   return (
     <>
-      <Home/>
+      <Expform/>
     </>
   );
 }
